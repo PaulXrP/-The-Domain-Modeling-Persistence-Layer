845db35dfcf50ca3c7f986d7d3757929d7861311
PaulXrP <pranaypaul@icloud.com>
2025-04-19 11:04:39 +0530

    Initial commit with user-passport-demo service

---
+++ user-passport-demo/src/main/java/com/dev/pranay/user_passport_demo/UserPassportDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.pranay.user_passport_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPassportDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPassportDemoApplication.class, args);
+	}
+
+}
---
+++ user-passport-demo/src/test/java/com/dev/pranay/user_passport_demo/UserPassportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.pranay.user_passport_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPassportDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
